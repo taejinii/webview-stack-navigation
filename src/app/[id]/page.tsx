75f4ad772290b039093413cf341cb15d33e3242d
taejinii <xowls177@gmail.com>
2024-11-10 01:50:08 +0900

    PostDetailPage delay remove

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -8,9 +8,6 @@ interface Params {
 export default async function PostDetail({ params }: { params: Promise<Params> }) {
     const { id } = await params;
 
-    // delay
-    await new Promise((resolve)=>setTimeout(resolve, 1000));
-
     const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`);
     const post: Post = await response.json();
 
