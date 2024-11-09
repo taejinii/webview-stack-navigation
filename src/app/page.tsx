f20a119ee79a6a2a0147c772bfb52e8bec869ca1
taejinii <xowls177@gmail.com>
2024-11-09 20:51:12 +0900

    bridge test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,9 @@ import {useRouter} from "next/navigation";
 export default function Home() {
   const router = useRouter()
   return (
-      <div className='h-screen flex items-center justify-center'>
+      <div className='flex flex-col'>
         <button onClick={()=>router.push('my-page')}>Move To Page</button>
+          <button className='text-sky-700 p-2 bg-blue-200  rounded-md' onClick={()=>window.webkit.actionHandler.postMessage('test')}>IOS Called</button>
       </div>
   );
 }
