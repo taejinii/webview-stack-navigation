96e239a654a8b8fe3f8799d01b42da3dfdbf1b0a
taejinii <xowls177@gmail.com>
2024-11-09 23:57:51 +0900

    post detail 페이지 params await 문구 추가

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -6,7 +6,7 @@ interface Params {
 }
 
 export default async function PostDetail({ params }: { params: Params }) {
-    const { id } = params;
+    const { id } = await params;
     const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`);
     const post: Post = await response.json();
 
