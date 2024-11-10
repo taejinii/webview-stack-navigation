ed44ca8518493a6352cf968e6da2469901fd180e
taejinii <xowls177@gmail.com>
2024-11-10 18:39:38 +0900

    wip

--- src/app/number/1/page.tsx
+++ src/app/number/1/page.tsx
@@ -1,10 +1,10 @@
 import NativePushButton from "@/app/number/native-push-button";
+import Link from "next/link";
 
 export default function One(){
     return <div className='flex flex-col gap-1'>
         <h1 className='text-4xl font-extrabold'>1 Page</h1>
         <NativePushButton number={2}/>
-        <NativePushButton number={3}/>
-        <NativePushButton number={4}/>
+        <Link href="/">Home</Link>
     </div>
 }
