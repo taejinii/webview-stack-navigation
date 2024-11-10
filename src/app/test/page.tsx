c97baeb7f716c282028cd10b8171c50c541e21ab
taejinii <xowls177@gmail.com>
2024-11-10 15:47:05 +0900

    테스트 페이지 뒤로가기 네이티브 백버튼 추가

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,9 +1,12 @@
 import HomeButton from "@/app/test/home-button";
+import BackButton from "@/app/[id]/back-button";
 
 export default function Test(){
-    return <div>
-        <div>뒤로가기 테스트</div>
-        <div>최초 화면으로 돌아가야함.</div>
-        <HomeButton/>
-    </div>
+    return (
+        <div className='flex flex-col gap-1'>
+            <div>뒤로가기 테스트</div>
+            <div>최초 화면으로 돌아가야함.</div>
+            <HomeButton/>
+            <BackButton/>
+        </div>)
 }
